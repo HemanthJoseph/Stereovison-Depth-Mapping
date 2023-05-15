3a033875f2b7efd4cd893a5ecfd280572e433e45
HemanthJoseph <joseph.hemanth@yahoo.com>
2023-05-15 16:07:47 -0400

    Adding Project files

---
+++ README.md
@@ -0,0 +1,144 @@
+<!-- # Download the files to a location of choice
+## Unzip it and you will find
+__
+|-- hemanth1.pdf (PDF report)
+|-- curule.py (python file for first dataset)
+|-- octagon.py (python file for second dataset)
+|-- pendulum.py (python file for third dataset)
+|-- This README.md file
+
+## Open the .py files one after the another in a IDE of choice, preferrably VSCode
+- Since the dataset files aren't a part of the ZIP file, in each python file change the path for the left image as img_0 in cv.imread
+- Similarly for the right image, change the path for the right image as img_1 in cv.imread
+### Run the files one after another by hitting the run button on the IDE and the program will run
+#### Note when running
+- As and when image outputs show on the screen, press any key to go to the next image output
+- The calculation for disparity takes anywhere between 3 - 6 minutes depending upon the computer specifications, please wait until then -->
+# Stereovision Depth Mapping
+Taking two images from a sequence, performing epipolar geometry on it. Then apply stereovision techniques to estimate the depth in the images. <br>
+This serves as an entry point for various 3-D depth estimation and scene reconstruction techniques. <br>
+
+Keypoints Image A         |  Keypoints Image B
+:-------------------------:|:-------------------------:
+![](./curule_image_outputs/keypoints_0.jpg)  |  ![](./curule_image_outputs/keypoints_1.jpg)
+
+## Pipeline to perform Stereovision Depth Mapping
+### Part 1 - Calibration of the camera
+1. Read two images separately and convert to grayscale for easier processing
+2. Rescale images to fit in screen
+3. Use ORB detector to detect keypoints in both images. Create an orb object
+4. Detect and compute key points and descriptors for both images
+5. Match features on both images. Create a Brute force matcher and use k-nearest neighbours
+method to match points on both images
+6. Now from the matched points we only choose good matches, ones which are distinct enough
+7. Now we need to calculate the pixel coordinates of the good matching points since the
+matching points are in the form of DObjects
+8. From the resulting coordinate points choose only the first 8 points for each image as we
+need only 8 points to calculate our Fundamental matrix
+9. Arrange these 8 points from each image into an array A and take its SVD
+10. The last column of the V matrix from the SVD is the needed Fundamental matrix. We
+extract and reshape into an 3 by 3 matrix and therefore we get the Fundamental Matrix
+11. Once we have the fundamental matrix we need to calculate the Essential Matrix. For that we
+need the intrinsic camera parameters, this is already provided to us
+12. The essential matrix is the matrix multiplication of the three matrices in the particular order
+Transpose of intrinsic matrix * Fundamental matrix * Intrinsic matrix
+13. Thus we obtain the Essential matrix for the images
+14. Now we need to decompose the Essential matrix into corresponding pairs of translation and
+rotation matrices. We will get four such pairs
+15. First we perform the SVD of Essential matrix. Define a new matrix W as follows
+```
+[0, -1, 0]
+[1, 0, 0]
+[0, 0, 1]
+```
+16. We get the following four pairs of translation and rotation matrices
+```
+C1 = U[:, 2]
+R1 = U * W * V transpose (U and V are from the SVD of essential matrix)
+C2 = - U[:, 2]
+R2 = U * W * V transpose
+C3 = U[:, 2]
+R3 = U * W transpose * V transpose
+C4 = - U[:, 2]
+R4 = U * W transpose * V transpose
+```
+
+Keypoints Image A         |  Keypoints Image B
+:-------------------------:|:-------------------------:
+![](./curule_image_outputs/keypoints_0.jpg)  |  ![](./curule_image_outputs/keypoints_1.jpg)
+
+
+Epilines Image A         |  Epilines Image B
+:-------------------------:|:-------------------------:
+![](./curule_image_outputs/epilines_0.jpg)  |  ![](./curule_image_outputs/epilines_1.jpg)
+
+
+### Part 2 – Rectification
+1. Using the cv.stereoRectifyUncalibrated function to get the homography matrices for
+both images
+The homography matrices are
+```
+H1 = [[-6.79073699e-01 -2.55442303e-03 2.50387638e+01]
+      [ 0.00000000e+00 -7.07110000e-01 0.00000000e+00]
+      [ 0.00000000e+00 0.00000000e+00 -7.07110000e-01]]
+
+H2 = [[1. 0. 0.]
+      [0. 1. 0.]
+      [0. 0. 1.]]
+```
+2. Warp the perspective of both the images so that the epilines are horizontal in the images
+
+Rectified Image A         |  Rectified Image B
+:-------------------------:|:-------------------------:
+![](./curule_image_outputs/rectified_0.jpg)  |  ![](./curule_image_outputs/rectified_1.jpg)
+
+### Part 3 – Correspondence and Depth Computation
+1. Convert the images into arrays
+2. If shape isn’t same raise an error to handle the exception
+3. Create a zeros of disparity map
+4. Since disparity calculation takes time, tqdm is used to monitor progress
+5. Iterate over each pixel to calculate disparity
+6. Choose a small block in left image
+7. Compare it with a block from the right image
+8. Get a search range for right image
+9. Iterate through the range for right image
+10. Calculate the disparity and depth
+
+<p align="center">
+    <img src="./curule_image_outputs/depth_image.png">
+
+These outputs are for just one dataset. Outputs from other datasets are available in the respective folders.
+
+## Usage
+* Clone the repo to your local machine
+```
+git clone [insert url here]
+```
+* Run the python file of choice
+```
+python curule.py
+```
+
+## Dependencies and libraries
+1. Python 3.9.12
+2. OpenCV '4.7.0'
+3. Numpy '1.24.2'
+
+## Note when running
+- As and when image outputs show on the screen, press any key to go to the next image output
+- The calculation for disparity takes anywhere between 3 - 6 minutes depending upon the computer specifications, please wait until then for the computation
+
+## Issues faced
+I faced an issue where my epipole was falling inside my image and I wasn’t getting the results I had expected. After going through eash line I found out the problem was with the bruteforce matcher method before getting the matching points. I was using the 
+```cv.NORM_HAMMING``` for matching which gave rise this problem.
+
+<p align="center">
+    <img src="Picture1.png"> <br>
+
+```
+bf = cv.BFMatcher_create(cv.NORM_HAMMING)
+```
+I solved this by using a simple brute force matcher.
+```
+bf = cv.BFMatcher()
+```
---
+++ curule.py
@@ -0,0 +1,289 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import *
+
+def rescaleFrame(frame, scale = 0.5):
+    width = int(frame.shape[1] * scale) #.shape[1] refers to width of image, typecasting it into integer value
+    height = int(frame.shape[0] * scale) #.shape[0] refers to height of image
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
+
+def drawlines(img1, img2, lines, pts1, pts2):
+    
+    # r, c = img1.shape
+    r = img1.shape[0]
+    c = img1.shape[1]
+    img1 = cv.cvtColor(img1, cv.COLOR_GRAY2BGR)
+    img2 = cv.cvtColor(img2, cv.COLOR_GRAY2BGR)
+      
+    for r, pt1, pt2 in zip(lines, pts1, pts2):
+          
+        color = tuple(np.random.randint(0, 255, 3).tolist())
+          
+        x0, y0 = map(int, [0, -r[2] / r[1] ])
+        x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1] ])
+          
+        img1 = cv.line(img1, (x0, y0), (x1, y1), color, 1)
+        img1 = cv.circle(img1, tuple(pt1), 5, color, -1)
+        img2 = cv.circle(img2, tuple(pt2), 5, color, -1)
+    return img1, img2
+
+def sum_of_abs_diff(pixel_vals_1, pixel_vals_2): #function to calculate the sum of absolute differences
+    if pixel_vals_1.shape != pixel_vals_2.shape:
+        return -1
+
+    return np.sum(abs(pixel_vals_1 - pixel_vals_2))
+
+def compare_blocks(y, x, block_left, right_array, block_size=5, SEARCH_BLOCK_SIZE=56):
+    # Get search range for the right image
+    x_min = max(0, x - SEARCH_BLOCK_SIZE)
+    x_max = min(right_array.shape[1], x + SEARCH_BLOCK_SIZE)
+    first = True
+    min_sad = None #sum of absolute differences
+    min_index = None
+    for x in range(x_min, x_max):
+        block_right = right_array[y: y+block_size,
+                                  x: x+block_size]
+        sad = sum_of_abs_diff(block_left, block_right)
+        if first:
+            min_sad = sad
+            min_index = (y, x)
+            first = False
+        else:
+            if sad < min_sad:
+                min_sad = sad
+                min_index = (y, x)
+
+    return min_index
+
+def main():
+
+    img_0 = cv.imread("curule/im0.png") #reading left image
+    resized_img_0 = rescaleFrame(img_0)
+    gray_0 = cv.cvtColor(resized_img_0, cv.COLOR_BGR2GRAY)
+
+    img_1 = cv.imread("curule/im1.png") #reading right image
+    resized_img_1 = rescaleFrame(img_1)
+    gray_1 = cv.cvtColor(resized_img_1, cv.COLOR_BGR2GRAY)
+
+    #Using ORB detector to extract the keypoints
+    #creating ORB Detector
+    orb = cv.ORB_create(nfeatures = 450) #by default it takes 500 as max no. of features
+
+    key_points_1, descriptors_1 = orb.detectAndCompute(gray_0, None)
+    key_points_2, descriptors_2 = orb.detectAndCompute(gray_1, None)
+
+
+    # Draw matches on each image
+    imgmatches_1 = cv.drawKeypoints(resized_img_0, key_points_1, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv.imshow("Image 0 Keypoints", imgmatches_1)
+    cv.imwrite("curule_image_outputs/keypoints_0.jpg", imgmatches_1)
+    cv.waitKey(0)
+
+    imgmatches_2 = cv.drawKeypoints(resized_img_1, key_points_2, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv.imshow("Image 1 Keypoints", imgmatches_2)
+    cv.imwrite("curule_image_outputs/keypoints_1.jpg", imgmatches_2)
+    cv.waitKey(0)
+
+    #Now, we match the features common to both images
+    #Create a bf(Brute Force) Matcher
+    # bf = cv.BFMatcher_create(cv.NORM_HAMMING) #cv.NORM_HAMMING is used for distance measurement
+    bf = cv.BFMatcher()
+
+    #compare and match descriptors from image 1 with image 2
+    matching_points = bf.knnMatch(descriptors_1, descriptors_2, k=2)  #two nearnest neighbours
+
+
+    #Finding out good matches, i.e., getting rid of point which aren't distinct enough
+    good_matching_points = []
+
+    for m,n in matching_points:
+        if m.distance < 0.6 * (n.distance):
+            good_matching_points.append(m)
+
+    #good matching points gives a set of DObjects of the matching points in each case, we need to extract pixel coordinates of matching
+    #points in both images
+
+    # Initialize lists to store the coordinates from each image
+    list_key_points_1 = []
+    list_key_points_2 = []
+
+    # Get the matching keypoints for each of the images
+    # Get the point coordinates
+    # Append the coordinates to each respective list
+
+
+    list_key_points_1 = [key_points_1[mat.queryIdx].pt for mat in good_matching_points] 
+    list_key_points_2 = [key_points_2[mat.trainIdx].pt for mat in good_matching_points]
+
+    #choosing eight points from first image and getting their x and y coordinates
+    x1_1 = list_key_points_1[0][0] #x1
+    y1_1 = list_key_points_1[0][1] #y1
+    x1_2 = list_key_points_1[1][0] #x2
+    y1_2 = list_key_points_1[1][1] #y2
+    x1_3 = list_key_points_1[2][0] #x3
+    y1_3 = list_key_points_1[2][1] #y3
+    x1_4 = list_key_points_1[3][0] #x4
+    y1_4 = list_key_points_1[3][1] #y4
+    x1_5 = list_key_points_1[4][0] #x5
+    y1_5 = list_key_points_1[4][1] #y5
+    x1_6 = list_key_points_1[5][0] #x6
+    y1_6 = list_key_points_1[5][1] #y6
+    x1_7 = list_key_points_1[6][0] #x7
+    y1_7 = list_key_points_1[6][1] #y7
+    x1_8 = list_key_points_1[7][0] #x8
+    y1_8 = list_key_points_1[7][1] #y8
+
+    #choosing eight points from second image and getting their x and y coordinates
+    x2_1 = list_key_points_2[0][0] #x1'
+    y2_1 = list_key_points_2[0][1] #y1'
+    x2_2 = list_key_points_2[1][0] #x2'
+    y2_2 = list_key_points_2[1][1] #y2'
+    x2_3 = list_key_points_2[2][0] #x3'
+    y2_3 = list_key_points_2[2][1] #y3'
+    x2_4 = list_key_points_2[3][0] #x4'
+    y2_4 = list_key_points_2[3][1] #y4'
+    x2_5 = list_key_points_2[4][0] #x5'
+    y2_5 = list_key_points_2[4][1] #y5'
+    x2_6 = list_key_points_2[5][0] #x6'
+    y2_6 = list_key_points_2[5][1] #y6'
+    x2_7 = list_key_points_2[6][0] #x7'
+    y2_7 = list_key_points_2[6][1] #y7'
+    x2_8 = list_key_points_2[7][0] #x8'
+    y2_8 = list_key_points_2[7][1] #y8'
+
+    #Now we use these coordinates from both images to create a matrix A which will help us calculate the fundamental matrix
+    A = np.array([
+        [x1_1*x2_1, x1_1*y2_1, x1_1, y1_1*x2_1, y1_1*y2_1, y1_1, x2_1, y2_1, 1],
+        [x1_2*x2_2, x1_2*y2_2, x1_2, y1_2*x2_2, y1_2*y2_2, y1_2, x2_2, y2_2, 1],
+        [x1_3*x2_3, x1_3*y2_3, x1_3, y1_3*x2_3, y1_3*y2_3, y1_3, x2_3, y2_3, 1],
+        [x1_4*x2_4, x1_4*y2_4, x1_4, y1_4*x2_4, y1_4*y2_4, y1_4, x2_4, y2_4, 1],
+        [x1_5*x2_5, x1_5*y2_5, x1_5, y1_5*x2_5, y1_5*y2_5, y1_5, x2_5, y2_5, 1],
+        [x1_6*x2_6, x1_6*y2_6, x1_6, y1_6*x2_6, y1_6*y2_6, y1_6, x2_6, y2_6, 1],
+        [x1_7*x2_7, x1_7*y2_7, x1_7, y1_7*x2_7, y1_7*y2_7, y1_7, x2_7, y2_7, 1],
+        [x1_8*x2_8, x1_8*y2_8, x1_8, y1_8*x2_8, y1_8*y2_8, y1_8, x2_8, y2_8, 1]])
+
+    #taking the SVD of A matrix to get the fundamental matrix
+    U, S, V = np.linalg.svd(A)
+    f_values = V[-1,:] #last column values of V are the f values which form the fundamental matrix
+    # f_values_array = np.array(f_values)
+    Fundamental_matrix = np.reshape(f_values,(3,3)) #reshaping to 3*3 dimensions
+    Fundamental_matrix = np.round(Fundamental_matrix , decimals =5)
+
+    # print(Fundamental_matrix)
+
+    #Now lets calculate the essential matrix
+    #for that we need the intrinsic matrix for the camera which is provided to us
+    K_matrix = np.array([
+        [1758.23, 0, 977.42],
+        [0, 1758.23, 552.15],
+        [0, 0, 1]])
+
+    #Essential Matrix
+    KT_matrix = K_matrix.transpose() # K transpose
+    Essential_matrix = np.matmul(np.matmul(KT_matrix, Fundamental_matrix), K_matrix)
+
+    # print(Essential_matrix)
+    # Now we decompose the essential matrix into its respective rotational and translational matrices
+    # So for that we need to take the SVD of Essential matrix
+    U, S, V = np.linalg.svd(Essential_matrix)
+
+    W = np.array([
+        [0, -1, 0],
+        [1, 0, 0],
+        [0, 0, 1]])
+
+    #We get four combinations of the cameras' translation and Rotation matrices
+    C1 = U[:,2]
+    R1 = np.matmul(np.matmul(U, W), V.transpose())
+
+    C2 = -1 * U[:,2]
+    R2 = np.matmul(np.matmul(U, W), V.transpose())
+
+    C3 = U[:,2]
+    R3 = np.matmul(np.matmul(U, W.transpose()), V.transpose())
+
+    C4 = -1 * U[:,2]
+    R4 = np.matmul(np.matmul(U, W.transpose()), V.transpose())
+
+
+
+    list_key_points_1 = np.int32(list_key_points_1)
+    list_key_points_2 = np.int32(list_key_points_2)
+
+    linesLeft = cv.computeCorrespondEpilines(list_key_points_2.reshape(-1, 1, 2), 2, Fundamental_matrix)
+    linesLeft = linesLeft.reshape(-1, 3)
+    img5, img6 = drawlines(gray_0, gray_1, linesLeft, list_key_points_1, list_key_points_2)
+
+
+    linesRight = cv.computeCorrespondEpilines(list_key_points_1.reshape(-1, 1, 2), 1, Fundamental_matrix)
+    linesRight = linesRight.reshape(-1, 3)
+    
+    img3, img4 = drawlines(gray_1, gray_0, linesRight, list_key_points_2, list_key_points_1)
+
+
+    cv.imshow("epilines 0", img5)
+    cv.imwrite("curule_image_outputs/epilines_0.jpg", img5)
+    cv.waitKey(0)
+    cv.imshow("epilines 1", img3)
+    cv.imwrite("curule_image_outputs/epilines_1.jpg", img3)
+    cv.waitKey(0)
+
+
+    #### Part 2 - Rectification ####
+    h1, w1 = resized_img_0.shape[0], resized_img_0.shape[1]
+    h2, w2 = resized_img_1.shape[0], resized_img_1.shape[1]
+
+    #this function will return homography matrices for both images
+    _, H1, H2 = cv.stereoRectifyUncalibrated(np.float32(list_key_points_1), np.float32(list_key_points_2), Fundamental_matrix, imgSize = (w1, h1))
+
+    print("H1 = ")
+    print(H1)
+    print("H2 = ")
+    print(H2)
+    #now we will use this homography matrices to warp the perspectives in both images
+    img_0_rectified = cv.warpPerspective(gray_0, H1, (w1, h1))
+    img_1_rectified = cv.warpPerspective(gray_1, H2, (w2, h2))
+
+    cv.imshow("img0 rectified", img_0_rectified)
+    cv.imwrite("curule_image_outputs/rectified_0.jpg", img_0_rectified)
+    cv.waitKey(0)
+    cv.imshow("img1 rectified", img_1_rectified)
+    cv.imwrite("curule_image_outputs/rectified_1.jpg", img_1_rectified)
+    cv.waitKey(0)
+
+
+    #### Part 3 - Correspondence and depth ####
+
+
+
+    #first we convert the image to arrays
+    left_im_array = np.asarray(img_0_rectified)
+    right_im_array = np.asarray(img_1_rectified)
+    left_array = left_im_array.astype(int)
+    right_array = right_im_array.astype(int)
+
+    if left_array.shape != right_array.shape: #exception error if shape is not same
+        raise "Left-Right image shape mismatch!"
+
+    h, w = left_array.shape
+    disparity_map = np.zeros((h, w)) #creating a disparity map of all zeros
+
+    BLOCK_SIZE = 20 #Block of pixels width and height
+    SEARCH_BLOCK_SIZE = 56 #size of the searching block
+
+    # Go over each pixel position
+    for y in tqdm(range(BLOCK_SIZE, h-BLOCK_SIZE)):
+        for x in range(BLOCK_SIZE, w-BLOCK_SIZE):
+            block_left = left_array[y:y + BLOCK_SIZE, x:x + BLOCK_SIZE] #subsection of left image as block
+            min_index = compare_blocks(y, x, block_left, right_array, block_size=BLOCK_SIZE, SEARCH_BLOCK_SIZE=SEARCH_BLOCK_SIZE)
+            disparity_map[y, x] = abs(min_index[1] - x)
+
+    plt.imshow(disparity_map, cmap='hot', interpolation='bilinear')
+    plt.savefig('curule_image_outputs/depth_image.png')
+    plt.show()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
